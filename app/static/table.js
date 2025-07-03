80b883f304490a769d20a283a88ebc7547f70356
Charlie <ctbushman@gmail.com>
2025-07-02 23:37:30 -0400

    refactor datatable init

---
+++ app/static/table.js
@@ -0,0 +1,9 @@
+function createDataTable(selector) {
+  return $(selector).DataTable({
+    ordering: false,
+    pageLength: 20,
+    lengthChange: false,
+    pagingType: 'full_numbers',
+    stateSave: true,
+  });
+}
