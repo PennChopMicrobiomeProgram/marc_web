4639f19dcc8e0b70d1e4247b7ecdc9b6ba14b621
Charlie <ctbushman@gmail.com>
2025-11-12 16:06:21 -0500

    Add development environment banner

--- app/app.py
+++ app/app.py
@@ -39,6 +39,14 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 
+IS_DEV_SITE = os.environ.get("MARC_DEV", "").lower() == "true"
+
+
+@app.context_processor
+def inject_environment_flags():
+    return {"is_dev_site": IS_DEV_SITE}
+
+
 # This line is only used in production mode on a nginx server, follow instructions to setup forwarding for
 # whatever production server you are using instead. It's ok to leave this in when running the dev server.
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
