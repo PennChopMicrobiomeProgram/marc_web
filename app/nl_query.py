8c4778e966ae7129d7ace450e2c2bd16daf6d7ff
Charlie <ctbushman@gmail.com>
2026-01-31 14:25:14 -0500

    Switch species tree to Archaeopteryx.js

--- app/nl_query.py
+++ app/nl_query.py
@@ -53,7 +53,8 @@ query_prompt_template = ChatPromptTemplate.from_messages(
     ]
 )
 
-GENERATE_QUERY_PROMPT = lambda user_input: f"""
+GENERATE_QUERY_PROMPT = (
+    lambda user_input: f"""
 ``` SYSTEM
 Given an input question, create a syntactically correct SQLite3 query to run to help find the answer. Unless the user specifies
 in his question a specific number of examples they wish to obtain, you can return all the results that match the question.
@@ -69,6 +70,7 @@ Only use the following tables:
 {user_input}
 ```
 """
+)
 
 llm = ChatOpenAI(
     model="gpt-4o",
